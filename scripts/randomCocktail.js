b2833eb576c908a182b7a302193cd7b7a82d527c
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-03-13 13:17:31 +0000

    Dev: Basic output from random call to API constructed.

---
+++ scripts/randomCocktail.js
@@ -0,0 +1,63 @@
+let request = new XMLHttpRequest();
+
+/* Get a random cocktail */
+request.open('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/random.php', true);
+
+request.onload = function(){
+    if(this.status === 200){
+        let cocktailData = JSON.parse(this.responseText);
+        console.log(cocktailData);
+
+        /* Get the name and the image of the cocktail */
+        let cocktailName = cocktailData.drinks[0]["strDrink"];
+        let cocktailImage = cocktailData.drinks[0]["strDrinkThumb"];
+
+        /* Set the arrays to contain the ingredients and measurements */
+        const cocktailIngredients = [];
+        const cocktailMeasurements = [];
+        let cocktailIndividualIngredient = '';
+        let cocktailIngredientMeasure = '';
+        
+        /* go through the properties of the object*/
+        for (let property in cocktailData.drinks[0]){
+        
+        /* Select all properties that start with strIngredient and push in to cocktailIngredients array */
+        if(property.startsWith('strIngredient') && cocktailData.drinks[0][property] !== null){
+            cocktailIndividualIngredient = cocktailData.drinks[0][property]; 
+            cocktailIngredients.push(cocktailIndividualIngredient);
+        }
+        /* Select all properties that start with strMeasure and push in to ocktailMeasurements array */
+        if(property.startsWith('strMeasure') && cocktailData.drinks[0][property] !== null){
+            cocktailIngredientMeasure = cocktailData.drinks[0][property];
+            cocktailMeasurements.push(cocktailIngredientMeasure);
+        }      
+        }
+
+        console.log(cocktailIngredients);
+        console.log(cocktailMeasurements);
+
+        /* Loop through the cocktail ingredients and created list items that includes the corresponding measurement */
+        /* ***FIX*** Not all ingredients have a measurement need to be able to exclude these as they output "undefined" */
+        let i = 0;
+        let ingredientsHTML = '';
+        for (i; i < cocktailIngredients.length; i++) {
+            ingredientsHTML += `<li>${cocktailMeasurements[i]} ${cocktailIngredients[i]}</li>`;
+        }
+            
+        /* Get the instructions for making the cocktial */
+        let cocktailInstructions = cocktailData.drinks[0]["strInstructions"];
+
+        /* Construct the HTML to view in index.html */
+        let cocktailHtml = `
+        <h1 class="h1">${cocktailName}</h1>
+        <img src="${cocktailImage}" alt="${cocktailName}">
+        <ul>${ingredientsHTML}</ul>
+        <p>${cocktailInstructions}</p>
+        `;
+            
+        /* Pass the HTML to the div the-data in  */
+        document.getElementById("the-data").innerHTML = cocktailHtml;
+
+    };
+};
+request.send();
