d2f224911bc3e678537d986efcb0831f61371fae
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-03-15 17:52:56 +0000

    Dev: Main.js now clicks through a list of 10 drinks returned json

--- backup-main.js
+++ backup-main.js
@@ -1,16 +1,20 @@
 let request = new XMLHttpRequest();
 
 /* Get a random cocktail */
-request.open('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/random.php', true);
+request.open('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php', true);
 
 request.onload = function(){
     if(this.status === 200){
         let cocktailData = JSON.parse(this.responseText);
         console.log(cocktailData);
 
+function loadNewCocktail() {
+    let i = 0
+
+    for (i; i < 10; i++) {
         /* Get the name and the image of the cocktail */
-        let cocktailName = cocktailData.drinks[0]["strDrink"];
-        let cocktailImage = cocktailData.drinks[0]["strDrinkThumb"];
+        let cocktailName = cocktailData.drinks[i]["strDrink"];
+        let cocktailImage = cocktailData.drinks[i]["strDrinkThumb"];
 
         /* Set the arrays to contain the ingredients and measurements */
         const cocktailIngredients = [];
@@ -19,16 +23,16 @@ request.onload = function(){
         let cocktailIngredientMeasure = '';
         
         /* go through the properties of the object*/
-        for (let property in cocktailData.drinks[0]){
+        for (let property in cocktailData.drinks[i]){
         
         /* Select all properties that start with strIngredient and push in to cocktailIngredients array */
-        if(property.startsWith('strIngredient') && cocktailData.drinks[0][property] !== null){
-            cocktailIndividualIngredient = cocktailData.drinks[0][property]; 
+        if(property.startsWith('strIngredient') && cocktailData.drinks[i][property] !== null){
+            cocktailIndividualIngredient = cocktailData.drinks[i][property]; 
             cocktailIngredients.push(cocktailIndividualIngredient);
         }
         /* Select all properties that start with strMeasure and push in to ocktailMeasurements array */
-        if(property.startsWith('strMeasure') && cocktailData.drinks[0][property] !== null){
-            cocktailIngredientMeasure = cocktailData.drinks[0][property];
+        if(property.startsWith('strMeasure') && cocktailData.drinks[i][property] !== null){
+            cocktailIngredientMeasure = cocktailData.drinks[i][property];
             cocktailMeasurements.push(cocktailIngredientMeasure);
         }      
         }
@@ -42,26 +46,32 @@ request.onload = function(){
         let ingredientsHTML = '';
         for (ingredientList; ingredientList < cocktailIngredients.length; ingredientList++) {
             if (cocktailMeasurements[ingredientList] === undefined && cocktailIngredients !== undefined) {
-                ingredientsHTML += `<li>${cocktailIngredients[ingredientList]}</li>`;
+                ingredientsHTML += `<li class="list-group-item">${cocktailIngredients[ingredientList]}</li>`;
             } else {
             if (cocktailMeasurements[ingredientList] !== undefined && cocktailIngredients !== undefined);
-            ingredientsHTML += `<li>${cocktailMeasurements[ingredientList]} ${cocktailIngredients[ingredientList]}</li>`;
+            ingredientsHTML += `<li class="list-group-item">${cocktailMeasurements[ingredientList]} ${cocktailIngredients[ingredientList]}</li>`;
             }
         }
         /* Get the instructions for making the cocktial */
-        let cocktailInstructions = cocktailData.drinks[0]["strInstructions"];
+        let cocktailInstructions = cocktailData.drinks[i]["strInstructions"];
 
         /* Construct the HTML to view in index.html */
         let cocktailHtml = `
         <h1 class="h1">${cocktailName}</h1>
         <img src="${cocktailImage}" alt="${cocktailName}">
-        <ul>${ingredientsHTML}</ul>
+        <ul class="list-group">${ingredientsHTML}</ul>
         <p>${cocktailInstructions}</p>
         `;
             
         /* Pass the HTML to the div the-data in  */
         document.getElementById("the-data").innerHTML = cocktailHtml;
+    }
+}
+    
+let nextCocktail = document.getElementById('clickNext');
+nextCocktail.addEventListener('click', loadNewCocktail);
 
     };
 };
+
 request.send();
---
+++ scripts/main.js
@@ -0,0 +1,79 @@
+let request = new XMLHttpRequest();
+
+/* Get a random cocktail */
+request.open('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php', true);
+
+request.onload = function(){
+    if(this.status === 200){
+        let cocktailData = JSON.parse(this.responseText);
+        console.log(cocktailData);
+
+        let drinkIndex = 0;
+
+        function loadNewCocktail() {           
+                
+            /* Get the name and the image of the cocktail */
+            let cocktailName = cocktailData.drinks[drinkIndex]["strDrink"];
+            let cocktailImage = cocktailData.drinks[drinkIndex]["strDrinkThumb"];
+
+            /* Set the arrays to contain the ingredients and measurements */
+            const cocktailIngredients = [];
+            const cocktailMeasurements = [];
+            let cocktailIndividualIngredient = '';
+            let cocktailIngredientMeasure = '';
+            
+            /* go through the properties of the object*/
+            for (let property in cocktailData.drinks[drinkIndex]){
+            
+            /* Select all properties that start with strIngredient and push in to cocktailIngredients array */
+            if(property.startsWith('strIngredient') && cocktailData.drinks[drinkIndex][property] !== null){
+                cocktailIndividualIngredient = cocktailData.drinks[drinkIndex][property]; 
+                cocktailIngredients.push(cocktailIndividualIngredient);
+            }
+            /* Select all properties that start with strMeasure and push in to ocktailMeasurements array */
+            if(property.startsWith('strMeasure') && cocktailData.drinks[drinkIndex][property] !== null){
+                cocktailIngredientMeasure = cocktailData.drinks[drinkIndex][property];
+                cocktailMeasurements.push(cocktailIngredientMeasure);
+            }      
+            }
+
+            console.log(cocktailIngredients);
+            console.log(cocktailMeasurements);
+
+            /* Loop through the cocktail ingredients and created list items that includes the corresponding measurement */
+            /* handle measurements that are "undefined" due to fewer entries in array */
+            let ingredientList = 0;
+            let ingredientsHTML = '';
+            for (ingredientList; ingredientList < cocktailIngredients.length; ingredientList++) {
+                if (cocktailMeasurements[ingredientList] === undefined && cocktailIngredients !== undefined) {
+                    ingredientsHTML += `<li class="list-group-item">${cocktailIngredients[ingredientList]}</li>`;
+                } else {
+                if (cocktailMeasurements[ingredientList] !== undefined && cocktailIngredients !== undefined);
+                ingredientsHTML += `<li class="list-group-item">${cocktailMeasurements[ingredientList]} ${cocktailIngredients[ingredientList]}</li>`;
+                }
+            }
+            /* Get the instructions for making the cocktial */
+            let cocktailInstructions = cocktailData.drinks[drinkIndex]["strInstructions"];
+
+            /* Construct the HTML to view in index.html */
+            let cocktailHtml = `
+            <h1 class="h1">${cocktailName}</h1>
+            <img src="${cocktailImage}" alt="${cocktailName}">
+            <ul class="list-group">${ingredientsHTML}</ul>
+            <p>${cocktailInstructions}</p>
+            `;
+                
+            /* Pass the HTML to the div the-data in  */
+            document.getElementById("the-data").innerHTML = cocktailHtml;
+
+            drinkIndex = drinkIndex + 1;
+                    
+            }   
+
+        let nextCocktail = document.getElementById('clickNext');
+        nextCocktail.addEventListener('click', loadNewCocktail)
+    }
+
+};
+
+request.send();
