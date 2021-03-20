b9d9e6de35ca71241105a59e2213e27f82bf0a7a
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-03-20 17:56:58 +0000

    Dev: Restructured to variables are accessible by multiple functions

--- scripts/getIngredients.js
+++ scripts/getIngredients.js
@@ -1,3 +1,11 @@
+let cocktailId = '';
+let cocktailName = '';
+let cocktailImage = '';
+let cocktailHtml = '';
+let drinkIndex = 0;
+let cocktailInstructions = '';
+let cocktailData = '';
+
 function getIngredientsURL() {
     let firstIngredient = document.getElementById("first-selection").value;
     let APIURL = `https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=${firstIngredient}`;  
@@ -13,52 +21,52 @@ function callAPI(APIURL) {
     /* Get cocktails with the selected ingredients */
     request.onload = function(){
         if(this.status === 200){
-            let cocktailData = JSON.parse(this.responseText);
-            console.log(cocktailData);
-
-            let drinkIndex = 0;
-
-            function showCocktail() {
-                
-            /* Get the name and the image of the cocktail */
-            let cocktailId = cocktailData.drinks[drinkIndex]["idDrink"];
-            let cocktailName = cocktailData.drinks[drinkIndex]["strDrink"];
-            let cocktailImage = cocktailData.drinks[drinkIndex]["strDrinkThumb"];
-
-            /* Construct the HTML to view output */
-            let cocktailHtml = `
-            <div id="select-cocktail">
-            <img src="${cocktailImage}" alt="${cocktailName}" class="drinks-image">
-            <h2 id="cocktail-id">${cocktailId}</h2>
-            <h1 class="h1 cocktail-title">${cocktailName}</h1>
-            <button id="click-next">Next</button>
-            <button id="show-how">Show me how to make this</button>
-            </div>                      
-            `;
-                
-            /* Pass the HTML to the div the-data and increment drinkIndex  */
-            document.getElementById("the-ingredient-data").innerHTML = cocktailHtml;
-            drinkIndex = drinkIndex + 1;
-
-            let nextCocktail = document.getElementById('click-next');
-            nextCocktail.addEventListener('click', showCocktail);
-            let showHowCocktails = document.getElementById('show-how');
-            showHowCocktails.addEventListener('click', getHow);
-
-            };
-            showCocktail();            
+            cocktailData = JSON.parse(this.responseText);
+            console.log(cocktailData);                   
         };    
     };
+    request.send();
+    showCocktail();     
+}
 
-    /* get how to make the cocktail */
-    function getHow() {
+/* show the selection of cocktail */
+function showCocktail() {
+        
+    /* Get the name and the image of the cocktail */
+    cocktailId = cocktailData.drinks[drinkIndex]["idDrink"];
+    cocktailName = cocktailData.drinks[drinkIndex]["strDrink"];
+    cocktailImage = cocktailData.drinks[drinkIndex]["strDrinkThumb"];
 
-    let cocktailIdToMake = document.getElementById('cocktail-id').textContent;
-    console.log(cocktailIdToMake);
+    /* Construct the HTML to view output */
+    cocktailHtml = `
+    <div id="select-cocktail">
+    <img src="${cocktailImage}" alt="${cocktailName}" class="drinks-image">
+    <h2 id="cocktail-id">${cocktailId}</h2>
+    <h2 id="cocktail-position">${drinkIndex}</h2>
+    <h1 class="h1 cocktail-title">${cocktailName}</h1>
+    <button id="click-next" onclick="showCocktail()">Next</button>
+    <button id="show-how">Show me how to make this</button>
+    </div>                      
+    `;
+        
+    /* Pass the HTML to the div the-data and increment drinkIndex  */
+    document.getElementById("the-ingredient-data").innerHTML = cocktailHtml;
+    drinkIndex = drinkIndex + 1;
+
+
+    let showHowCocktails = document.getElementById('show-how');
+    showHowCocktails.addEventListener('click', getHow);
+
+};
+
+/* get how to make the cocktail */
+function getHow() {
+    cocktailPositionInArray = document.getElementById('cocktail-position').textContent;
+
 
     let howToRequest = new XMLHttpRequest();
 
-    howToRequest.open('GET', `https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=${cocktailIdToMake}`, true);
+    howToRequest.open('GET', `https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=${cocktailId}`, true);
 
     howToRequest.onload = function(){
         if(this.status === 200){
@@ -66,9 +74,7 @@ function callAPI(APIURL) {
             console.log(cocktailHowToData);
         
             /* Get the name and the image of the cocktail to make */
-            let cocktailHowToName = cocktailHowToData.drinks[0]["strDrink"];
-            let cocktailHowToImage = cocktailHowToData.drinks[0]["strDrinkThumb"];
-            let cocktailHowToInstructions = cocktailHowToData.drinks[0]["strInstructions"];
+            cocktailInstructions = cocktailHowToData.drinks[0]["strInstructions"];
 
             /* Set the arrays to contain the ingredients and measurements */
             const cocktailIngredients = [];
@@ -88,8 +94,8 @@ function callAPI(APIURL) {
             if(property.startsWith('strMeasure') && cocktailHowToData.drinks[0][property] !== null){
                 cocktailIngredientMeasure = cocktailHowToData.drinks[0][property];
                 cocktailMeasurements.push(cocktailIngredientMeasure);
-            }      
-            }
+            };    
+            };
 
             /* Loop through the cocktail ingredients and created list items that includes the corresponding measurement */
             /* handle measurements that are "undefined" due to fewer entries in array */
@@ -101,34 +107,28 @@ function callAPI(APIURL) {
                 } else {
                 if (cocktailMeasurements[ingredientList] !== undefined && cocktailIngredients !== undefined);
                 ingredientsHowToHtml += `<li>${cocktailMeasurements[ingredientList]} ${cocktailIngredients[ingredientList]}</li>`;
-                }
-            }
+                };
+            };
 
         /* Construct the HTML to view how to make the cocktail */
             let cocktailToMakeHtml = `
             <div id="selected-cocktail">
-            <img src="${cocktailHowToImage}" alt="${cocktailHowToName}" class="drinks-image-how">
-            <h1 class="h1">${cocktailHowToName}</h1>
+            <img src="${cocktailImage}" alt="${cocktailName}" class="drinks-image-how">
+            <h1 class="h1">${cocktailName}</h1>
+            <h1 class="h2">Cocktail ID = ${cocktailId}</h1>           
             <ul>${ingredientsHowToHtml}</ul>
-            <p>${cocktailHowToInstructions}</p>
-            <button id="go-back">Go Back</button>
+            <p>${cocktailInstructions}</p>
+            <button id="go-back" onclick="showCocktail()">Go Back</button>
             </div>
             `;
                 
             /* Pass the HTML to the div the-ingredient-data */
             document.getElementById("the-ingredient-data").innerHTML = cocktailToMakeHtml;
-        }
-    }
+
+        };
+    };
     howToRequest.send();
-}
 
+};
 
-
-
-    request.send();
-}
-
-
-let haveIngredientsCocktails = document.getElementById('show-me');
-haveIngredientsCocktails.addEventListener('click', getIngredientsURL)
 
