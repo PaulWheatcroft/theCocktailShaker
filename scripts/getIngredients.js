8c4cb7d66a0c7038433dc86e2ffea02a6378f7bf
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-03-27 18:51:32 +0000

    Dev: Added more ingredients. Minor CSS change ahead of merging to master

--- scripts/getIngredients.js
+++ scripts/getIngredients.js
@@ -1,26 +1,19 @@
 function filterFirstIngredient() {
-    let input, filter, li, i, txtValue;
+    let input, filter, li, i, textValue;
     input = document.getElementById("first-selection");
     filter = input.value.toUpperCase();
     li = document.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        
-        txtValue = li[i].textContent || li[i].innerText;
-
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    for (i = 0; i < li.length; i++) {        
+        textValue = li[i].innerText;
+        if (textValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
         };
     };
 }
-
-let ingredientListItem;
-
-
-
 function listenIngredientList() {
-    ingredientListItem = document.getElementsByClassName('drinks-list');
+    let ingredientListItem = document.getElementsByClassName('drinks-list');
     for (let i = 0; i<ingredientListItem.length; i++) {
         ingredientListItem[i].addEventListener("click", ingredientName);
             function ingredientName() {
@@ -28,11 +21,10 @@ function listenIngredientList() {
             document.getElementById('first-selection').value = theIngredient;
         }
     }
-
 }
+
+
 listenIngredientList();
-
-
 
 let cocktailId = '';
 let cocktailName = '';
