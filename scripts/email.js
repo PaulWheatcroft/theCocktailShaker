c1d70894086bc01e644ff267c62fd5c30c0a2081
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-04-05 18:36:49 +0000

    Dev: Email instructions now sent through EmailJS

--- scripts/email.js
+++ scripts/email.js
@@ -1,9 +1,3 @@
-function startEmailjs() {
-    emailjs.init("user_xuHqhg0d940gXwsJXVA5A");
-};
-
-startEmailjs();
-
 
 let cocktailName = localStorage.getItem('cocktailName');
 let cocktailIngredients = localStorage.getItem('cocktailIngredients');
@@ -17,15 +11,14 @@ console.log(cocktailName);
 console.log(cocktailIngredients); 
 console.log(cocktailInstructions);
 
-
-
 function formSubmit() {
-    emailjs.sendForm(
-        "service_r1s6k5e",
-        "template_464i5ds",
-        "#email_form",
-        "user_xuHqhg0d940gXwsJXVA5A"
-    )
+    emailjs.sendForm("service_r1s6k5e", "template_464i5ds", "#email-form", "user_xuHqhg0d940gXwsJXVA5A")
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
 }
 
-document.getElementById('send-instructions').click = formSubmit();
+const sendForm = document.getElementById('send-instructions');
+sendForm.addEventListener('click', formSubmit);
