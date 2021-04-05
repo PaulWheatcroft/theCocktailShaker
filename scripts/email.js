0e434779ae031f5da0c95200e6b523a3a4e470dd
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-04-05 16:26:12 +0000

    Dev: EmailJS installed.

--- scripts/email.js
+++ scripts/email.js
@@ -1,3 +1,10 @@
+function startEmailjs() {
+    emailjs.init("user_xuHqhg0d940gXwsJXVA5A");
+};
+
+startEmailjs();
+
+
 let cocktailName = localStorage.getItem('cocktailName');
 let cocktailIngredients = localStorage.getItem('cocktailIngredients');
 let cocktailInstructions = localStorage.getItem('cocktailInstructions');
@@ -11,3 +18,14 @@ console.log(cocktailIngredients);
 console.log(cocktailInstructions);
 
 
+
+function formSubmit() {
+    emailjs.sendForm(
+        "service_r1s6k5e",
+        "template_464i5ds",
+        "#email_form",
+        "user_xuHqhg0d940gXwsJXVA5A"
+    )
+}
+
+document.getElementById('send-instructions').click = formSubmit();
