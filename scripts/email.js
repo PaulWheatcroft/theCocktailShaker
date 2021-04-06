86014edd9123203fb0baee8a3caded49024a8a75
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-04-06 19:49:41 +0000

    Dev: Send email journey completed

--- scripts/email.js
+++ scripts/email.js
@@ -1,3 +1,5 @@
+document.getElementById("information-container").className = 'send-email';
+
 
 let cocktailName = localStorage.getItem('cocktailName');
 let cocktailIngredients = localStorage.getItem('cocktailIngredients');
@@ -11,15 +13,27 @@ console.log(cocktailName);
 console.log(cocktailIngredients); 
 console.log(cocktailInstructions);
 
+
+
 function formSubmit() {
     event.preventDefault();
     emailjs.sendForm("service_r1s6k5e", "template_464i5ds", "#email-form", "user_xuHqhg0d940gXwsJXVA5A")
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
+       document.getElementById("information-container").className = 'enjoy';
+       let sentHtml = `
+       <h1>Enjoy!</h1>
+       <p>That has been sent to you. I do hope you enjoy making it. And I do hope you enjoy drinking it twice as much!</p>
+       <p>Bottoms up. xx</p>
+       <button id="choose-again" class="buttons pink-button" ><a href="index.html">I fancy another go</a></button>
+       `;
+        document.getElementById("information-container").innerHTML = sentHtml;
     }, function(error) {
        console.log('FAILED...', error);
     });
 }
+
+
 
 const sendForm = document.getElementById('send-instructions');
 sendForm.addEventListener('click', formSubmit);
