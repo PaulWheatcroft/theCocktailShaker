23c3737276c9a40920e60d621976916cf4019c17
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-04-06 17:43:37 +0000

    Fix: Default form behavior disabled. Form now sends correctly.

--- scripts/email.js
+++ scripts/email.js
@@ -12,6 +12,7 @@ console.log(cocktailIngredients);
 console.log(cocktailInstructions);
 
 function formSubmit() {
+    event.preventDefault();
     emailjs.sendForm("service_r1s6k5e", "template_464i5ds", "#email-form", "user_xuHqhg0d940gXwsJXVA5A")
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
