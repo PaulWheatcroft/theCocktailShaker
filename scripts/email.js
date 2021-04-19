d33ec3ef204c81456dae01cae8b42f07804c2d10
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-04-19 22:15:52 +0000

    Dev: Error messaging to ensure valid ingredients

--- scripts/email.js
+++ scripts/email.js
@@ -14,23 +14,24 @@ document.getElementById('text-instructions').innerText = cocktailInstructions;
 /* ----------- if the email is successfully sent the page will change to complete the journey */
 /* ----------- if the email fails then the user will recieve an alert */
 function formSubmit() {
+    document.getElementById('error-message').className = 'hidden-error-message animate__animated animate__fadeOut';
     event.preventDefault();
     let sentHtml = '';
     emailjs.sendForm("service_r1s6k5e", "template_464i5ds", "#email-form", "user_xuHqhg0d940gXwsJXVA5A")
     .then(function(response) {
         console.log('SUCCESS!', response.status, response.text);
-        document.getElementById("information-container").className = 'enjoy animate__animated animate__fadeIn';
         sentHtml = `
-            <h1>Enjoy!</h1>
-            <p>That has been sent to you, you may need to check your junk folder. I hope you enjoy making it. And I do hope you enjoy drinking it twice as much!</p>
-            <p>Bottoms up. xx</p>
-            <button id="choose-again" class="buttons pink-button" ><a href="index.html">I fancy another go</a></button>
+        <h1>Enjoy!</h1>
+        <p>That has been sent to you, you may need to check your junk folder. I hope you enjoy making it. And I do hope you enjoy drinking it twice as much!</p>
+        <p>Bottoms up. xx</p>
+        <button id="choose-again" class="buttons pink-button" ><a href="index.html">I fancy another go</a></button>
         `;
         document.getElementById("information-container").innerHTML = sentHtml;
+        document.getElementById("information-container").className = 'enjoy animate__animated animate__fadeIn';
     },
     function(error) {
-        console.log('FAILED...', error);
-        alert("Please enter a valid email address");
+        document.getElementById('error-message').innerHTML = `<p>Please enter a valid email address</p>`;
+        document.getElementById('error-message').className = 'visible-error-message animate__animated animate__fadeIn';
         return;
     });
 }
